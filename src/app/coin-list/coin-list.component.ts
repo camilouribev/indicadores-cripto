5d357f3494f07fac18a96e56b6a05dd90aeb3b25
camilo.uribev <camilo.uribev@sofka.com.co>
2023-09-12 12:33:38 -0500

    Creation of api service

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
   styleUrls: ['./coin-list.component.scss']
 })
-export class CoinListComponent {
+export class CoinListComponent implements OnInit {
+  
+  constructor( private api: ApiService){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllData();
+    this.getBannerData();
+  }
+
+  getBannerData(){
+    this.api.getTrendingCurrency('COP')
+    .subscribe( response =>{console.log(response)} 
+    )}
+
+  getAllData(){
+    this.api.getCurrency("COP").subscribe( response =>{
+      console.log(response);
+    })
+  }
+
+  
 
 }
