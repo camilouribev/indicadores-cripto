1126d0f8e90f04c44a9deecc498255b3a2b577f0
camilo.uribev <camilo.uribev@sofka.com.co>
2023-09-12 11:33:24 -0500

    Toolbar and creation of coin-list and coin-detail components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'indicadores-cripto';
+  selectedCurrency: string = "COP"
+  constructor(){
+
+  }
+  sendCurrency(event:string){
+    console.log(event);
+    
+  }
 }
