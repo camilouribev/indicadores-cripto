b58bee41efc77b9c9d6969dd64c8f05904b579cf
camilo.uribev <camilo.uribev@sofka.com.co>
2023-09-13 09:15:00 -0500

    Fixed tags in detail view component

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coin-detail',
   templateUrl: './coin-detail.component.html',
   styleUrls: ['./coin-detail.component.scss']
 })
-export class CoinDetailComponent {
+export class CoinDetailComponent implements OnInit {
+
+  coinData : any;
+  coinId! : string;
+  days : number = 1;
+  currency : string = "EUR"
+
+  constructor(private api : ApiService, private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( value =>{
+      this.coinId = value['id']
+    });
+
+    this.getCoinData();
+  }
+
+  getCoinData(){
+    this.api.getCurrencyById(this.coinId)
+    .subscribe( response => {
+      this.coinData = response;
+      console.log(this.coinData);
+      
+    })
+  }
 
 }
