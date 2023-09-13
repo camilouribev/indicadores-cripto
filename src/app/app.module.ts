ac3fc0e9dbb9471a81fda5900e2d9b5b071a45b6
camilo.uribev <camilo.uribev@sofka.com.co>
2023-09-13 11:00:34 -0500

    Created graph area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatSortModule} from '@angular/material/sort';
+import { NgChartsModule } from 'ng2-charts';
 
 
 
@@ -38,7 +39,8 @@ import {MatSortModule} from '@angular/material/sort';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { ActivatedRoute } from '@angular/router';
+import { ChartConfiguration, ChartType} from 'chart.js'
+import { BaseChartDirective } from 'ng2-charts'; 
 
 @Component({
   selector: 'app-coin-detail',
@@ -14,6 +16,39 @@ export class CoinDetailComponent implements OnInit {
   days : number = 1;
   currency : string = "EUR"
 
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+
+      }
+    ],
+    labels: []
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
+
   constructor(private api : ApiService, private activatedRoute: ActivatedRoute){}
 
   ngOnInit(): void {
@@ -22,6 +57,7 @@ export class CoinDetailComponent implements OnInit {
     });
 
     this.getCoinData();
+    this.getGraphData();
   }
 
   getCoinData(){
@@ -31,6 +67,24 @@ export class CoinDetailComponent implements OnInit {
       console.log(this.coinData);
       
     })
+  }
+
+  getGraphData(){
+    this.api.getGraphicalCurrencyData(this.coinId, "EUR", 1)
+    .subscribe( response =>{
+      setTimeout(()=>{this.myLineChart.chart?.update();}, 200);
+      this.lineChartData.datasets[0].data = response.prices.map(( a: any) =>{
+        return a[1];
+      })
+      this.lineChartData.labels = response.prices.map((a:any) =>{
+        let date = new Date(a[0]);
+        let time = date.getHours() > 12 ?
+        `${date.getHours() - 12}: ${date.getMinutes()} PM` :
+        `${date.getHours() - 12}: ${date.getMinutes()} AM`
+        return this.days === 1 ? time : date.toLocaleTimeString();
+      })
+    })
+
   }
 
 }
